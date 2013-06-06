6de6f7a8685545b00bc0df232bafcfcf398bf623
Russell Harmon <russ@eatnumber1.com>
2013-06-06 09:54:13 -0700

    [travis] Add output to the test.

--- .travis/run.sh
+++ .travis/run.sh
@@ -5,5 +5,7 @@ setopt err_exit no_unset warn_create_global
 
 git ls-files -z -- '.zsh*' | while read -rd $'\0' file; do
 	[[ $file =~ \.zsh/help/.* ]] && continue
+	echo -n "Syntax checking $file... "
 	zsh -o no_exec $file
+	echo "OK"
 done
