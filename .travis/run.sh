7c85e7d9888ff48591bfa8a3f7b132c65de5f6b6
Russell Harmon <russ@eatnumber1.com>
2013-06-06 09:51:08 -0700

    [travis] Add travis-ci

---
+++ .travis/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env zsh
+emulate -L zsh
+setopt err_exit no_unset warn_create_global
+setopt xtrace
+
+git ls-files -z -- '.zsh*' | while read -rd $'\0' file; do
+	[[ $file =~ \.zsh/help/.* ]] && continue
+	zsh -o no_exec $file
+done
